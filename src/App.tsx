1210d56ac327706f745fa9d56716a0b30bb0b68e
IvanNycheporuk <ivan.nycheporuk@gmail.com>
2022-04-21 20:25:46 +0300

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Build Mafia App :)
         </a>
       </header>
     </div>
